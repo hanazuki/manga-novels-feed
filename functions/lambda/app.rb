4d0ba98c4fbbc100ca8b9497afd14b72bd1cec14
Kasumi Hanazuki <kasumi@rollingapple.net>
2020-01-21 05:47:29 +0000

    mangacross.jp

--- functions/lambda/app.rb
+++ functions/lambda/app.rb
@@ -61,10 +61,47 @@ module Providers
       end
     end
   end
+
+  class MangaCross
+    BASE_URI = URI('https://mangacross.jp')
+
+    def rss(id)
+      info = JSON.parse(
+        Net::HTTP.get(
+          URI("https://mangacross.jp/api/comics/#{id}.json"),
+        )
+      )
+
+      RSS::Maker.make('2.0') do |maker|
+        maker.channel.title = info['comic']['title']
+        maker.channel.link = "https://mangacross.jp/comics/#{id}"
+        maker.channel.description = info['comic']['seo_outline']
+
+        maker.items.do_sort = true
+
+        info['episodes'].each do |episode|
+          next if episode['status'] == 'private'
+
+          unless published_at = episode['public_start'] || episode['member_publish_start']
+            next
+          end
+
+          maker.items.new_item do |item|
+            url = item.link = BASE_URI + episode['page_url']
+            item.title = [episode['volume'], episode['title']].join(?\s).strip
+            item.date = Time.parse(published_at)
+            item.guid.content = url
+            item.guid.isPermaLink = true
+          end
+        end
+      end
+    end
+  end
 end
 
 $providers = {
   'magnet-novels.com' => Providers::MagnetNovels.new,
+  'mangacross.jp' => Providers::MangaCross.new,
 }
 
 def handler(event:, context:)
